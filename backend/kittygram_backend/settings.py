c25d45ce2a7b82fe03c0cc1d52c36f6eb075fcc1
Trukhova <antru231812@list.ru>
2023-07-31 23:11:20 +0300

    itt1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -55,11 +55,18 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       # бэкенд postgresql
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+       'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
+       'HOST': os.getenv('DB_HOST', ''),
+       'PORT': os.getenv('DB_PORT', 5432)
+   }
 }
+
+
 
 
 # Password validation
@@ -93,8 +100,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "collected_static"
+
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
