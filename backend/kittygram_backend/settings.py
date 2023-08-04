7b4d3186d44a42b26c3e0a0ad51cd7696fd021c4
Trukhova <antru231812@list.ru>
2023-08-04 12:48:47 +0300

    fix after 2 review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = bool(os.getenv('DEBUG', 'False').lower())
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ypkittygramm.hopto.org']
 
